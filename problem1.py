cff56035a1d1e7985888634b0d79d6bbfe8a11da
Caitlyn Tan <caitlyn.tan23@ycdsbk12.ca>
2021-03-03 16:03:49 +0000

    submit

--- problem1.py
+++ problem1.py
@@ -1,1 +1,36 @@
+'''
+-------------------------------------------------------------------------------
+Name:		main.py
 
+Purpose:	determines which group a player is placed in
+
+Author:	Tan.C
+
+Created:	03/03/2021
+------------------------------------------------------------------------------
+'''
+print("****** Tournament Tracker ******")
+print("")
+
+# variable start value
+w_count = 0
+
+# get number of wins and losses from user
+print("Enter the wins and losses for your team: ")
+for i in range(6):
+  user_results = input("")
+  if user_results == "w" or "W":
+    w_count = w_count + 1
+
+# output results
+if w_count == 5 or 6:
+  print("Your team is in Group 1")
+
+elif w_count == 3 or 4:
+  print("Your team is in Group 2")
+
+elif w_count == 1 or 2:
+  print("Your team is in Group 3")
+
+else:
+  print("Your team is eliminated from the tournament")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,32 @@
+'''
+-------------------------------------------------------------------------------
+Name:		main.py
 
+Purpose:	allows user to enter the distance driven for each day until the total distance driven surpasses 100km
+
+Author:	Tan.C
+
+Created:	03/03/2021
+------------------------------------------------------------------------------
+'''
+print("***** Welcome to the DoorDash Distance Tracker ******")
+print("")
+
+# variable start values
+total = 0
+days = 0
+distance = 0
+
+# get distances from user
+print("** Travel Log **")
+while total <= 100:
+  distance = int(input("Enter the distance travelled for the day: "))
+  total = total + distance
+  days += 1
+
+# output number of days and average distance
+print("")
+print("** Summary **")
+average = total/days
+print("It took " + str(days) + " days to surpass 100km driven.")
+print("The average distance driven per day is " + str(average) + " km")
